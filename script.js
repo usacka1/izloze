7db9001f2660eb21e59743b34e174b7f3d20fe3a
usacka1 <uusacka@gmail.com>
2022-03-08 10:18:18 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,5 @@
+const vardi = ["Jana Krūmiņa", "Jānis Bērziņš", "Pēteris Liepa", "Laura Ose", "Artis Priede"];
+const balvas = ["Dators", "Telefons", "Kafijas automāts", "Fotoaparāts", "Automašīna"];
+const naudaKopa = 1000000;
+let uzvaretajuSkiats = 5;
+console.log(vardi[rand]);
