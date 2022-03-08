64fa01dcfb78b0f5980566bdacbe0d5ad90a664d
usacka1 <uusacka@gmail.com>
2022-03-08 11:14:38 +0200

    Stundas darbs

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ const vardi = ["Jana Krūmiņa", "Jānis Bērziņš", "Pēteris Liepa", "Laura O
 const balvas = ["Dators", "Telefons", "Kafijas automāts", "Fotoaparāts", "Automašīna"];
 const naudaKopa = 1000000;
 let uzvaretajuSkiats = 5;
-console.log(vardi[rand]);
+let rindas = document.querySelector(".rindas");
+for (let i=0; i<uzvaretajuSkiats; i++){
+    let rand = Math.random()*vardi.length;
+    rand = Math.floor(rand);
+    //noapaļo uz leju
+    let uzvaretajs = vardi[rand];
+}
+rindas.innerHTML +=`
+//pievieno +. lai pieskaitītu nākamo
+<tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>`
